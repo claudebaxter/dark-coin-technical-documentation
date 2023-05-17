e52ec730d5c5296ded56b2eb01005053b191a022
elborracho420 <e.baxter@dark-coin.io>
2023-05-17 01:14:55 -0500

    Setting up responsive page w/static navbar &dynamic content window w/ better png assets.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="nav-container">
+        <div className="navbar">
+          <h1>Navbar Content</h1>
+          <img className="icon1" src="/Icon-new.png"></img>
+        </div>
+      </div>
+      <div className="content">
+          <div className="triangletop"></div>
+          <img className="icon2" src="/Icon-old.png"></img>
+          <h1 className="title">Dark Coin Technical Documentation</h1>
+          <div className="trianglebottom"></div>
+      </div>
+      
       
     </div>
   );
