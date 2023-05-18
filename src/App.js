67379ea4bbca44436a6ad05b1b87c4ca9a7a3785
elborracho420 <e.baxter@dark-coin.io>
2023-05-17 22:51:42 -0500

    Adding socials icons to navbar and adjusting positioning. Adding border shadow to splash pulse effect.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,24 @@ function App() {
       <div className="nav-container">
         <div className="navbar">
           <h1>Navbar Content</h1>
-          <img className="icon1" src="/Icon-new.png"></img>
+          <img className="icon1" src="/Icon-old.png"></img>
+          <div className="soc-container">
+            <img className="socials" src="/discord-icon.png"></img>
+            <img className="socials" src="/vestige-icon.png"></img>
+            <img className="socials" src="/email-icon.png"></img>
+            <img className="socials" src="/linkedin-icon.png"></img>
+          </div>
+          <div className="soc-container2">
+            <img className="socials" src="/reddit-icon.png"></img>
+            <img className="socials" src="/twitter-icon.png"></img>
+            <img className="socials" src="/github-icon.png"></img>           
+            <img className="socials" src="/youtube-icon.png"></img>
+          </div>
         </div>
       </div>
       <div className="content">
           <div className="triangletop"></div>
-          <img className="icon2" src="/Icon-old.png"></img>
+          <img className="icon2" src="/Icon-new.png"></img>
           <h1 className="title">Dark Coin Technical Documentation</h1>
           <div className="trianglebottom"></div>
       </div>
