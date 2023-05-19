b3b323547df98afa4a8893d9e6b503c4d0e03bab
elborracho420 <e.baxter@dark-coin.io>
2023-05-18 23:40:15 -0500

    Added links and link / button hover styles.

--- src/App.js
+++ src/App.js
@@ -40,23 +40,43 @@ function App() {
           <div className="navbutton" onClick={() => handleNavButtonClick('Darkpaper')}>
             <img className="navicon" src="/DarkPaper.png"></img> <p>darkPaper</p>
           </div>
-          <div className="navbutton">
-            <img className="navicon" src="/DarkRepo.png"></img> <p>darkRepo</p>
-          </div>
-          <div className="navbutton">
-            <img className="navicon" src="/Icon-new.png"></img> <p>App</p>
-          </div>
+          <a href="https://github.com/elborracho420/Dark-Coin-ASA-1088771340">
+            <div className="navbutton">
+              <img className="navicon" src="/DarkRepo.png"></img> <p>darkRepo</p>
+            </div>
+          </a>
+          <a href="https://dark-coin.com">
+            <div className="navbutton">
+              <img className="navicon" src="/Icon-new.png"></img> <p>App</p>
+            </div>
+          </a>
           <div className="soc-container">
-            <img className="socials" src="/discord-icon.png"></img>
-            <img className="socials" src="/vestige-icon.png"></img>
-            <img className="socials" src="/email-icon.png"></img>
-            <img className="socials" src="/linkedin-icon.png"></img>
+            <a href="https://discord.gg/xdZ6V5ybmq">
+              <img className="socials" src="/discord-icon.png"></img>
+            </a>
+            <a href="https://vestige.fi/asset/1088771340">
+              <img className="socials" src="/vestige-icon.png"></img>
+            </a>
+            <a href="mailto:team@dark-coin.io">
+              <img className="socials" src="/email-icon.png"></img>
+            </a>
+            <a href="https://www.linkedin.com/company/dark-coin-dao/">
+              <img className="socials" src="/linkedin-icon.png"></img>
+            </a>
           </div>
           <div className="soc-container2">
-            <img className="socials" src="/reddit-icon.png"></img>
-            <img className="socials" src="/twitter-icon.png"></img>
-            <img className="socials" src="/github-icon.png"></img>           
-            <img className="socials" src="/youtube-icon.png"></img>
+            <a href="https://www.reddit.com/r/DarkCoinASA/">
+              <img className="socials" src="/reddit-icon.png"></img>
+            </a>
+            <a href="https://twitter.com/DarkCoinASA">
+              <img className="socials" src="/twitter-icon.png"></img>
+            </a>
+            <a href="https://github.com/ToysToTinkerWith/DarkCoin">
+              <img className="socials" src="/github-icon.png"></img>
+            </a>
+            <a href="https://www.youtube.com/watch?v=o-YBDTqX_ZU">
+              <img className="socials" src="/youtube-icon.png"></img>
+            </a>  
           </div>
         </div>
       </div>
