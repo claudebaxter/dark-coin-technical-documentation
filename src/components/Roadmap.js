169aabf084b481d2639fa5715d6cc685cf3348a4
elborracho420 <e.baxter@dark-coin.io>
2023-05-20 13:19:05 -0500

    Writing basic jsx/html markup and styling Roadmap component layout.

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -1,9 +1,28 @@
 function Roadmap() {
     return(
         <div>
-            <div className="triangletop"></div>
-            <img className="icon2" src="/Icon-new.png"></img>
-            <h1 className="title">Dark Coin Roadmap</h1>
+            <div className="triangletoprm"></div>
+            <h1 className="titlerm">Dark Coin Roadmap 2022</h1>
+            <div className="line">
+                <div className="circle">Q1</div>
+                <div className="circle">Q2</div>
+                <div className="circle">Q3</div>
+                <div className="circle">Q4</div>
+            </div>
+            <h1 className="titlerm">Dark Coin Roadmap 2023</h1>
+            <div className="line">
+                <div className="circle">Q1</div>
+                <div className="circle">Q2</div>
+                <div className="circle">Q3</div>
+                <div className="circle">Q4</div>
+            </div>
+            <h1 className="titlerm">Dark Coin Roadmap 2024</h1>
+            <div className="line">
+                <div className="circle">Q1</div>
+                <div className="circle">Q2</div>
+                <div className="circle">Q3</div>
+                <div className="circle">Q4</div>
+            </div>
             <div className="trianglebottom"></div>
         </div>
     )
