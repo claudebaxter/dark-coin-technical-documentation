5ffcad1392339d69e9a2aff655521e3f79801758
claudebaxter <e.baxter@dark-coin.io>
2023-11-12 12:08:32 -0600

    Pushed back incomplete roadmap items Q3 to Q4

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -77,23 +77,17 @@ function Roadmap() {
                 <div className="circle" 
                     onClick={() => handleCircleClick({
                         title: "Q3 2023",
-                        description: "• 2023 Multi-Sig Election Scheduled",
-                        description2: "• DAO proposal: Arena Bossfights (Lottery)?",
-                        description3: "• DAO proposal: Game Project(s)?",
-                        description4: "• DAO proposal: Dark Coin xGov proposal?",
-                        description5: "• ",
                         description6: "• Tie in Council dApp to discord bot",
                         description7: "• Development of rewards claim dApp features",
-                        description8: "• ",
                     })}>Q3</div>
                 <div className="circle" 
                     onClick={() => handleCircleClick({
                         title: "Q4 2023",
-                        description: "• (action items pending)",
-                        description2: "• ",
-                        description3: "• ",
-                        description4: "• ",
-                        description5: "• ",
+                        description: "• DAO proposal: Arena Bossfights (Lottery)?",
+                        description2: "• 2023 Multi-Sig Election Scheduled",
+                        description3: "• DAO proposal: Game Project(s)?",
+                        description4: "• DAO proposal: Dark Coin xGov proposal?",
+                        description5: "• DAO proposal: Dark Coin Warrior Tier 1 Remint in collaboration with Immortal Jolly",
                         description6: "• ",
                         description7: "• ",
                     })}>Q4</div>
